028e88fbbf704e534aaddebae487c4f341bd2558
Peter Yefi <peteryefi@gmail.com>
2022-06-01 20:49:05 -0400

    feat: Fixed formatting

--- release.config.js
+++ release.config.js
@@ -1,5 +1,9 @@
 module.exports = {
-    branches: "main",
-    repositoryUrl: "https://github.com/peteryefi/react_app",
-    plugins: ["@semantic-release/commit-analyzer", "@semantic-release/release-notes-generator", "@semantic-release/github"]
-}
+  branches: "main",
+  repositoryUrl: "https://github.com/peteryefi/react_app",
+  plugins: [
+    "@semantic-release/commit-analyzer",
+    "@semantic-release/release-notes-generator",
+    "@semantic-release/github",
+  ],
+};
