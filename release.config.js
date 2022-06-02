f46469e758b427c824a9814d9923d8fb9ea2d40f
Peter Yefi <peteryefi@gmail.com>
2022-06-01 20:46:28 -0400

    feat: Added release

--- release.config.js
+++ release.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    branches: "master",
+    branches: "main",
     repositoryUrl: "https://github.com/peteryefi/react_app",
     plugins: ["@semantic-release/commit-analyzer", "@semantic-release/release-notes-generator", "@semantic-release/github"]
 }
