d82777f213ead5c67809f955516733377712c443
Peter Yefi <peteryefi@gmail.com>
2022-06-03 22:16:55 -0400

    feat: update Issue

--- .github/actions/issue/index.js
+++ .github/actions/issue/index.js
@@ -1,20 +1,23 @@
 const core = require("@actions/core");
 const github = require("@actions/github");
+async function run() {
+  try {
+    const token = core.getInput("token");
+    const title = core.getInput("title");
+    const body = core.getInput("body");
+    const assignees = core.getInput("assignees");
+    const octokit = new github.getOctokit(token);
+    const response = await octokit.rest.issues.create({
+      ...github.context.repo,
+      title,
+      body,
+      assignees: assignees ? assignees.split("\n") : undefined,
+    });
 
-try {
-  const token = core.getInput("token");
-  const title = core.getInput("title");
-  const body = core.getInput("body");
-  const assignees = core.getInput("assignees");
-  const octokit = new github.getOctokit(token);
-  const response = octokit.rest.issues.create({
-    ...github.context.repo,
-    title,
-    body,
-    assignees: assignees ? assignees.split("\n") : undefined,
-  });
-
-  core.setOutput("issue", JSON.stringify(response.data));
-} catch (error) {
-  core.setFailed(error.message);
+    core.setOutput("issue", JSON.stringify(response.data));
+  } catch (error) {
+    core.setFailed(error.message);
+  }
 }
+
+run();
