2140b69cb2b4a490c9e459812a27bbca428f9bf4
Peter Yefi <peteryefi@gmail.com>
2022-06-03 22:01:59 -0400

    feat: create issue

--- .github/actions/hello/index.js
+++ .github/actions/hello/index.js
@@ -1,20 +1,19 @@
 const core = require("@actions/core");
 const github = require("@actions/github");
 try {
-
   //Enable debugging
-  core.debug('Debug message');
-  core.warning('Warning message');
-  core.error('Error message');
+  core.debug("Debug message");
+  core.warning("Warning message");
+  core.error("Error message");
   const name = core.getInput("who-to-greet");
   console.log(`Hello ${name}`);
 
   const time = new Date();
   core.setOutput("time", time.toTimeString());
-  core.startGroup('Logging Github Object');
+  core.startGroup("Logging Github Object");
   console.log(JSON.stringify(github, null, "\t"));
   core.endGroup();
-  core.exportVariable('HELLO', 'Hello, Mr. Yefi');
+  core.exportVariable("HELLO", "Hello, Mr. Yefi");
 } catch (error) {
   core.setFailed(error.message);
 }
